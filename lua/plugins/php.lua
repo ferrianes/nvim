57b969d1812c75af7e14f5a7131b78b1d17fbe16
Ferrian Septiawan <ferrian27@gmail.com>
2024-08-11 15:41:42 +0700

    add laravel pint as formatter php

---
+++ lua/plugins/php.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    -- Remove phpcs linter.
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        php = {},
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        intelephense = {
+          settings = {
+            intelephense = {
+              diagnostics = {
+                undefinedMethods = false,
+                undefinedTypes = false,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        php = { "pint" },
+      },
+    },
+  },
+}
